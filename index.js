fd9a02cb87f35e140a35860ee66bc8ec043af92c
loteknowledG <loteknowledG@gmail.com>
2021-03-25 13:46:31 -0400

    up

--- index.js
+++ index.js
@@ -17,25 +17,41 @@ const posts = [
   }
 ];
 const typeDefs = `
- type Query {
-  post(postId: Int!): Post
-  allPosts: [Post!]!
-}
-type Post {
-   id: Int
-   title: String
-   content: String
- }
+  type Query {
+    post(postId: Int!): Post
+    allPosts: [Post!]!
+  }
+  type Post {
+    id: Int
+    title: String
+    content: String
+  }
+  type Mutation {
+    addPost(title: String!, content: String!): Post
+  }
 `
 const resolvers = {
- Query: {
+  Query: {
     post: (_, { postId }, context, info) => {
-     return posts.filter(post => post.id === postId)[0];
+      return posts.filter(post => post.id === postId)[0];
     },
     allPosts: (_, args, context, info) => {
-     return posts;
+      return posts;
     }
   },
+  Mutation: {
+    addPost: (_, { title, content }, context, info) => {
+    const newPost = {
+        id: 4,
+        title,
+        content
+      }
+      posts.push(newPost)
+      return newPost;
+    }
+  }
 }
+
+
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log('Server is running on localhost:4000'));
