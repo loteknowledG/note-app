fc2e42010072c8c5ead1e4cfe1ba5afaaa4e37a5
loteknowledG <loteknowledG@gmail.com>
2021-03-25 08:03:17 -0400

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const { GraphQLServer } = require('graphql-yoga')
+const posts = [
+  {
+    id: 1,
+    title: 'Article one',
+    content: 'This is Article One'
+  },
+  {
+    id: 2,
+    title: 'Article Two',
+    content: 'This is Article Two'
+  },
+  {
+    id: 3,
+    title: 'Article Three',
+    content: 'This is Article Three'
+  }
+];
+const typeDefs = `
+ type Query {
+  post(postId: Int!): Post
+  allPosts: [Post!]!
+}
+type Post {
+   id: Int
+   title: String
+   content: String
+ }
+`
+const resolvers = {
+ Query: {
+    post: (_, { postId }, context, info) => {
+     return posts.filter(post => post.id === postId)[0];
+    },
+    allPosts: (_, args, context, info) => {
+     return posts;
+    }
+  },
+}
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log('Server is running on localhost:4000'));
